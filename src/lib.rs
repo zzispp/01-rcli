74a570aae2930e31d40c04cd66327e089d45d1bd
zwj <zzispp@gmail.com>
2025-05-05 17:06:28 +0800

    feature: add csv cli support

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+mod opts;
+mod process;
+
+pub use opts::{CsvOpts, Opts, SubCommand};
+
+pub use process::process_csv;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, Rust!");
+use anyhow::Ok;
+use clap::Parser;
+use rcli::process_csv;
+use rcli::{Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
